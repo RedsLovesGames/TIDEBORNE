096b19463b0ea9dbe44d0e2516b2b002fb3631f7
RedsLovesGames <75150137+RedsLovesGames@users.noreply.github.com>
2026-08-28 03:24:14 -0400

    Fix Apex shark mixin hierarchy

--- src/main/java/com/redslovesgames/tideboundcompatibility/mixin/apex/GreatWhiteSharkMixin.java
+++ src/main/java/com/redslovesgames/tideboundcompatibility/mixin/apex/GreatWhiteSharkMixin.java
@@ -10,25 +10,36 @@ import com.li64.tide.data.fishing.FishData;
 import com.redslovesgames.tideboundcompatibility.compat.apex.SharkFoodGoal;
 import com.redslovesgames.tideboundcompatibility.compat.apex.TideFishTargetGoal;
 import com.redslovesgames.tideboundcompatibility.config.TideboundConfig;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.WaterCreatureEntity;
-import net.minecraft.world.World;
+import net.minecraft.entity.ai.goal.GoalSelector;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/**
+ * Apex Waters is an optional external mod. Do not mirror its Minecraft superclass in
+ * this mixin: Loom remaps the external JAR between Mojmap/intermediary/Yarn namespaces,
+ * and a reconstructed source-level superclass can therefore fail Mixin hierarchy
+ * validation even when the target bytecode is compatible. Shadow the inherited goal
+ * selectors instead, which keeps the mixin namespace-stable.
+ */
 @Mixin(GreatWhiteSharkEntity.class)
-abstract class GreatWhiteSharkMixin extends WaterCreatureEntity {
-   protected GreatWhiteSharkMixin(EntityType<? extends WaterCreatureEntity> type, World level) {
-      super(type, level);
-   }
+abstract class GreatWhiteSharkMixin {
+   @Shadow
+   @Final
+   protected GoalSelector goalSelector;
+
+   @Shadow
+   @Final
+   protected GoalSelector targetSelector;
 
    @Inject(method = "registerGoals", at = @At("TAIL"))
    private void tidebound$registerFoodGoals(CallbackInfo callback) {
-      GreatWhiteSharkEntity shark = (GreatWhiteSharkEntity)this;
+      GreatWhiteSharkEntity shark = (GreatWhiteSharkEntity)(Object)this;
       this.goalSelector.add(2, new SharkFoodGoal(shark));
       this.targetSelector.add(2, new TideFishTargetGoal(shark));
    }
