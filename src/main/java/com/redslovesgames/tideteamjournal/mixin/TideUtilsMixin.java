00287292a73e5ece5499cb9c06dc1e4e3cf82c7c
RedsLovesGames <75150137+RedsLovesGames@users.noreply.github.com>
2026-09-04 13:02:14 -0400

    refactor: centralize Team Journal catch bridge

--- src/main/java/com/redslovesgames/tideteamjournal/mixin/TideUtilsMixin.java
+++ src/main/java/com/redslovesgames/tideteamjournal/mixin/TideUtilsMixin.java
@@ -5,37 +5,24 @@
  */
 package com.redslovesgames.tideteamjournal.mixin;
 
-import com.li64.tide.data.player.TidePlayerData;
 import com.li64.tide.util.TideUtils;
-import com.redslovesgames.tideteamjournal.TeamJournalService;
-import com.redslovesgames.tideteamjournal.TeamProgressStore;
+import com.redslovesgames.tideteamjournal.TeamJournalCatchBridge;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TideUtils.class)
 abstract class TideUtilsMixin {
-   @Unique
-   private static final ThreadLocal<TidePlayerData> TIDE_TEAM_JOURNAL_BEFORE_CATCH = new ThreadLocal<>();
-
    @Inject(method = "tryLogCatch", at = @At("HEAD"))
-   private static void tideTeamJournal$snapshotTeamJournal(ItemStack stack, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> callback) {
-      TeamProgressStore.tideborneBeginCatch(stack);
-      TIDE_TEAM_JOURNAL_BEFORE_CATCH.set(new TidePlayerData(TeamJournalService.loadFor(player).getAsTag()));
+   private static void tideTeamJournal$beginCatch(ItemStack stack, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> callback) {
+      TeamJournalCatchBridge.beginTryLog(stack, player);
    }
 
    @Inject(method = "tryLogCatch", at = @At("RETURN"))
-   private static void tideTeamJournal$captureAfterSavedCatch(ItemStack stack, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> callback) {
-      TidePlayerData before = TIDE_TEAM_JOURNAL_BEFORE_CATCH.get();
-      TIDE_TEAM_JOURNAL_BEFORE_CATCH.remove();
-      if (callback.getReturnValueZ() && before != null) {
-         TeamJournalService.captureCatchAfterSave(before, TeamJournalService.loadFor(player), player);
-      }
-
-      TeamProgressStore.tideborneClearCatch();
+   private static void tideTeamJournal$finishCatch(ItemStack stack, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> callback) {
+      TeamJournalCatchBridge.finishTryLog(callback.getReturnValueZ(), player);
    }
 }
