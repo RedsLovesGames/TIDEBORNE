e9a761d89a3546a9f2b717b3d3c1b46a9130f147
RedsLovesGames <75150137+RedsLovesGames@users.noreply.github.com>
2026-09-10 15:48:59 -0400

    P3: point ModMenu at canonical config screen

--- src/main/java/com/redslovesgames/tideborne/client/TideborneModMenu.java
+++ src/main/java/com/redslovesgames/tideborne/client/TideborneModMenu.java
@@ -8,8 +8,9 @@ package com.redslovesgames.tideborne.client;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
+/** Thin ModMenu adapter for Tideborne's single configuration screen. */
 public final class TideborneModMenu implements ModMenuApi {
    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-      return TideborneUnifiedConfigScreen::create;
+      return TideborneConfigScreen::create;
    }
 }
